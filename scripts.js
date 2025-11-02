a6f52a69725a937597bba17f7255d46d54fe18e7
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-11-01 21:48:03 -0300

    Tipos de Dados
    Os principais e mais usados

--- scripts.js
+++ scripts.js
@@ -21,8 +21,50 @@ if(1) {
     let y = 55
     console.log(y)
 }
-//Não podemos redeclarar uma variável menos que ela esteja no mesmo escopo.s
-
+//Não podemos redeclarar uma variável menos que ela esteja no mesmo escopo.
 console.log(x)
 console.log(y)
 console.log(z)
+
+
+// tipos de dados
+// Typeof Explica o  tipo de dado (boolean, string, null, objeto
+const name = 'Pablo'
+console.log(name)
+console.log(typeof name)
+
+const shirtsQty = 4
+console.log(shirtsQty)
+console.log(typeof shirtsQty)
+
+const decimal = 12.4
+console.log(decimal)
+console.log(typeof decimal)
+
+const isApproved = false //Verdadeiro ou Falso
+console.log(isApproved)
+console.log(typeof isApproved)
+
+let surname = null
+console.log(surname)
+console.log(typeof surname)
+
+surname = "Pablo"
+console.log( typeof surname)
+
+let age
+console.log(age)
+console.log(typeof age)
+
+age = 30
+console.log(age)
+console.log(typeof age)
+
+const languages = ['JavaScript', 'PHP', 'Python']
+console.log(languages)
+console.log(typeof languages)
+
+const user = {idade: '20',email: "pablocruz@gmail.com", password: 'teste123123'} //Objeto
+console.log(user)
+console.log(typeof user)
+
