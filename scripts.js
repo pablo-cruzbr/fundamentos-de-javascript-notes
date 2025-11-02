2e5cc10f54009654424d61ac97ad022804329eae
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-11-01 21:23:15 -0300

    Principais métodos console.log
    Usado para depurar código ou para enviar algum aviso

---
+++ scripts.js
@@ -0,0 +1,10 @@
+console.log("Hello Word JavaScript")
+
+//console
+//Usamos console para depurar nosso codigo ou para enviar algum aviso
+//menssagens, avisos, erros e ira ser impressão na aba console do navegador
+
+console.error("Este é um erro!")
+console.warn("Este é um aviso")
+console.log("Está é uma mensagens")
+
